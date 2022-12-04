52a9d0647929a67d3dfbc2d2d36bb75993f5249a
AnnaShliapkina <deer1anna@gmail.com>
2022-12-04 13:49:46 +0200

    create & fx

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,18 @@
 const express = require("express");
 
 const { validation, ctrlWrapper } = require("../../middlewares/middleWares");
-const { contactSchema } = require("../../schema/contacts");
+const { contactSchema, favoriteContactSchema } = require("../../models/index");
 const { contacts: ctrl } = require("../../controllers");
 const router = express.Router();
 const validateMiddleWareUpdate = validation(contactSchema, "This Field is missing");
 const validateMiddleWareAdd = validation(contactSchema, "You should put name in this field");
+const validateMiddleWarePatch = validation(contactSchema, "You should put name in this field");
 
 router.get("/", ctrlWrapper(ctrl.getAll));
 router.get("/:contactId", ctrlWrapper(ctrl.getById));
 router.post("/", validateMiddleWareAdd, ctrlWrapper(ctrl.add));
 router.delete("/:contactId", ctrlWrapper(ctrl.removeById));
 router.put("/:contactId", validateMiddleWareUpdate, ctrlWrapper(ctrl.updateById));
+router.patch("/:contactId/favorite", validateMiddleWarePatch, ctrlWrapper(ctrl.updateStatusContact));
 
 module.exports = router;
